63b9bd40d34b86f455fa7e2c14799571705c0b8d
Ericgacoki <gacokieric@gmail.com>
2022-12-05 00:29:38 +0300

    make tool bar transparent

--- app/src/main/java/com/ericdev/goshopping/app/MainActivity.kt
+++ app/src/main/java/com/ericdev/goshopping/app/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.ericdev.goshopping.app
 
+import android.R
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.ericdev.goshopping.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -11,8 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        hideStatusBar()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun hideStatusBar() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = ContextCompat.getColor(this, R.color.transparent)
     }
 }
