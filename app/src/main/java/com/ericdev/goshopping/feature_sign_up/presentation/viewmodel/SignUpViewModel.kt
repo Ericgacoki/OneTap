880822ee8adfd4dc4060dc49c6811de496a19e5e
Ericgacoki <gacokieric@gmail.com>
2022-12-20 14:49:59 +0300

    move viewmodel to package

--- app/src/main/java/com/ericdev/goshopping/feature_sign_up/presentation/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/ericdev/goshopping/feature_sign_up/presentation/viewmodel/SignUpViewModel.kt
@@ -1,45 +1,36 @@
-package com.ericdev.goshopping.feature_sign_up.presentation
+package com.ericdev.goshopping.feature_sign_up.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ericdev.goshopping.feature_sign_up.domain.model.SignUpState
-import com.ericdev.goshopping.feature_sign_up.domain.repository.SignUpRepository
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpViewModel @Inject constructor(
-    val auth: FirebaseAuth,
-    val repository: SignUpRepository
-) : ViewModel() {
+class SignUpViewModel @Inject constructor(private val auth: FirebaseAuth) : ViewModel() {
 
     var inputEmail = ""
     var inputPassword = ""
     var inputConfirmPassword = ""
 
-    private val _signUpState = MutableLiveData<SignUpState>(SignUpState.LOADING)
-    val signUpState get() = _signUpState
+    private val _signUpState: MutableLiveData<SignUpState> = MutableLiveData(SignUpState.LOADING)
+    val signUpState: LiveData<SignUpState> = _signUpState
 
     fun signUpUser() {
         viewModelScope.launch {
             auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
-                .addOnCompleteListener() { task ->
+                .addOnCompleteListener { task ->
                     _signUpState.value = if (task.isSuccessful) {
                         auth.currentUser?.sendEmailVerification()
                         SignUpState.SUCCESSFUL
-                    } else {
-                        SignUpState.FAILED
-                    }
+                    } else SignUpState.FAILED
+                }.addOnCanceledListener {
+                    _signUpState.value = SignUpState.FAILED
                 }
         }
-    }
-
-    fun checkUser(): FirebaseUser? {
-        return auth.currentUser
     }
 }
