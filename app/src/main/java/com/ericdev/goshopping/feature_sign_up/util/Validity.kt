100308d04ba059490cb81627ff850fb96d22a665
Ericgacoki <gacokieric@gmail.com>
2022-12-20 14:49:19 +0300

    add validity checker to global module util

--- app/src/main/java/com/ericdev/goshopping/feature_sign_up/util/Validity.kt
+++
@@ -1,6 +0,0 @@
-package com.ericdev.goshopping.feature_sign_up.util
-
-sealed class Validity(val message: String = ""){
-    object Valid: Validity()
-    data class InValid(val reason: String): Validity(reason)
-}
