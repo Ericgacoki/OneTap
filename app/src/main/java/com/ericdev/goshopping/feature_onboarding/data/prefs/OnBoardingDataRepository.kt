8c7e59bae073e026f62904017474350791daf0b5
Ericgacoki <gacokieric@gmail.com>
2022-12-22 11:53:43 +0300

    rename repo

---
+++ app/src/main/java/com/ericdev/goshopping/feature_onboarding/data/prefs/OnBoardingDataRepository.kt
@@ -0,0 +1,35 @@
+package com.ericdev.goshopping.feature_onboarding.data.prefs
+
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import com.ericdev.goshopping.feature_onboarding.domain.repository.OnBoardingRepository
+import com.ericdev.goshopping.util.Constants
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class OnBoardingDataRepository @Inject constructor(val app: Application) : OnBoardingRepository {
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "prefs")
+        private val FINISHED_ON_BOARDING = booleanPreferencesKey(Constants.FINISHED_ON_BOARDING)
+    }
+
+    override suspend fun setOnBoardingFinished(finished: Boolean) {
+        app.applicationContext.dataStore.edit { prefs ->
+            prefs[FINISHED_ON_BOARDING] = finished
+        }
+    }
+
+    override fun getOnBoardingFinished(): Flow<Boolean> {
+        val finishedOnBoarding: Flow<Boolean> = app.applicationContext.dataStore.data
+            .map { prefs ->
+                prefs[FINISHED_ON_BOARDING] ?: false
+            }
+        return finishedOnBoarding
+    }
+}
